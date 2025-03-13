1dd3d5bf12cb791d7a4f4b06371a93d789fb8a4f
Aline Fernandes <fernandes.dev@outlook.com>
2025-03-12 22:11:14 -0300

    Revert "Delete Processamento_De_Dados directory"
    This reverts commit 486f675832a7b92980d68cdb6446ed9653529484.

---
+++ Processamento_De_Dados/Program.cs
@@ -0,0 +1,64 @@
+﻿//Exercicios de fixação de conceitos de C#
+
+/* 
+int x, y;
+x = 5;
+y = 2 * x;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+
+Console.ReadLine();
+*/
+
+
+/*
+int x;
+double y;
+
+x = 5;
+y = 2 * x;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+*/
+
+
+/*
+double b, B, h, area;
+
+b = 6.0;
+B = 8.0;
+h = 5.0;
+
+area = (b + B) / 2.0 * h;
+
+Console.WriteLine(area);
+Console.ReadLine();
+*/
+
+
+/*
+int a, b;
+double resultado;
+
+a = 5;
+b = 2;
+
+resultado = (double) a / b;
+
+Console.WriteLine(resultado);
+
+Console.ReadLine();
+*/
+
+
+double a;
+int b;
+
+a = 5.0;
+b = (int) a;
+
+Console.WriteLine(b);
+
+Console.ReadLine();
