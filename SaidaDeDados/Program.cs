08470a0290bb7da497a720dee6bfc9989dcf2a55
Aline Fernandes <fernandes.dev@outlook.com>
2025-04-23 11:14:58 -0300

    ajuste na Concatenação

--- SaidaDeDados/Program.cs
+++ SaidaDeDados/Program.cs
@@ -1,12 +1,19 @@
-﻿using System.Xml;
+﻿using System.Globalization;
 
-int idade = 32;
-double saldo = 10.35784;
-string nome = "Maria";
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
 
-//Placeholders
-Console.WriteLine(  "{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
-//Interpolação
-Console.WriteLine(  $"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
-//Concatenando
-Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais ");
+            //Placeholders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+            //Concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a "
+                + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+    }
+}
