78fcb4a449786d27239b6e0f870ca0d0d82b9a06
Aline Fernandes <fernandes.dev@outlook.com>
2025-04-23 11:00:30 -0300

    Concatenação

--- SaidaDeDados/Program.cs
+++ SaidaDeDados/Program.cs
@@ -1,8 +1,12 @@
-﻿int idade = 32;
+﻿using System.Xml;
+
+int idade = 32;
 double saldo = 10.35784;
 string nome = "Maria";
 
 //Placeholders
 Console.WriteLine(  "{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
 //Interpolação
-Console.WriteLine(  $" {nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+Console.WriteLine(  $"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+//Concatenando
+Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais ");
