22a9c17085a68df291648f44730648762e0ba904
Aline Fernandes <fernandes.dev@outlook.com>
2025-05-14 10:47:46 -0300

    Calculo de area

---
+++ calculo-de-area/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Globalization;
+Console.WriteLine("Digite o raio do círculo: ");
+double raio = double.Parse(Console.ReadLine());
+double area = 3.14159 * raio * raio;
+
+Console.WriteLine("Area:" + area.ToString("F4", CultureInfo.InvariantCulture));
