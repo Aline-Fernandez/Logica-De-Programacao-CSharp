c2b4a03ca7c3a8803fe978a3767be19c662e461f
Aline Fernandes <fernandes.dev@outlook.com>
2025-05-14 10:00:04 -0300

    Calculo de soma

---
+++ entrada-e-saida-Exercicio01/Program.cs
@@ -0,0 +1,10 @@
+﻿int A, B, soma;
+
+Console.WriteLine("Digite dois números inteiros:");
+A = int.Parse(Console.ReadLine());
+B = int.Parse(Console.ReadLine());
+
+
+soma = A + B;
+
+Console.WriteLine($"SOMA = {soma}");
