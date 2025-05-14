abf5ef5596cd02708b6363dbc19d42b42b05589b
Aline Fernandes <fernandes.dev@outlook.com>
2025-05-14 10:47:46 -0300

    diferença de valoresCalculo de area

---
+++ calculo-diferença-de-valores/Program.cs
@@ -0,0 +1,10 @@
+﻿int a, b, c, d, diferenca;
+Console.WriteLine("Digite os valores");
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+c = int.Parse(Console.ReadLine());
+d = int.Parse(Console.ReadLine());
+
+ diferenca = (a * b) - (c * d);
+
+Console.WriteLine("DIFERENÇA = " +  diferenca);
