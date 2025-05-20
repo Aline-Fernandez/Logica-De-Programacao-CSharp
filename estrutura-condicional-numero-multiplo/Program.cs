a37b859104924e94c43c6edc1ef95aea1aa8c9c1
Aline Fernandes <fernandes.dev@outlook.com>
2025-05-20 17:56:11 -0300

    Consultar se número é multiplo

---
+++ estrutura-condicional-numero-multiplo/Program.cs
@@ -0,0 +1,17 @@
+﻿int a, b;
+
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine("Numero multiplo");
+}
+else if (b % a == 0)
+{
+    Console.WriteLine("Numero multiplo");
+}
+else
+{
+    Console.WriteLine("Numero não é multiplo");
+}
